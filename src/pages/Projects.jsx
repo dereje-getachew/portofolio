60c2dfb3b35b1a25b550c369b0b222418d41f798
derejegetachew12 <getachewdereje128@gmail.com>
2026-04-01 11:00:42 +0300

    fix

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,8 @@
-import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "../components/Projects/ProjectCard";
 import Particle from "../components/Particle";
-import pg from "../assets/projects/pg.png";
 import project from "../assets/projects/project.jpeg";
 import Spotify from "../assets/projects/Spotify.png";
-import krypto from "../assets/projects/krypto.png";
 import kickstart from "../assets/projects/kickstart.png";
 
 const Projects = () => {
@@ -45,7 +42,7 @@ const Projects = () => {
               imgPath={kickstart}
               isBlog={false}
               title="E‑Commerce Website"
-              description="Fanaye ERP is a stock management system built to streamline inventory tracking, reporting, and overall warehouse operations. Developed using Laravel, React, and MySQL, it delivers real-time insights and efficient stock control for businesses."
+              description="Fanaye ERP is a stocks management system built to streamline inventory tracking, reporting, and overall warehouse operations. Developed using Laravel, React, and MySQL, it delivers real-time insights and efficient stock control for businesses."
               ghLink="erp.fanayetechnologies.com"
             />
           </Col>
